380183fd75b1d231006e2cbc7a84908d1b4a09d8
Ole Mussmann <gitlab+account@ole.mn>
2023-09-28 16:15:29 +0200

    enable installation with Nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "nilm env";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      py = pkgs.python311Packages;
+    in {
+      devShells.default = pkgs.mkShell rec {
+        packages = with pkgs; [
+          py.ipywidgets
+          py.jupyterlab
+          py.pandas
+          py.plotly
+          py.pytorch
+          py.scikit-learn
+          py.tensorboard
+          py.tqdm
+        ];
+      };
+    });
+}
